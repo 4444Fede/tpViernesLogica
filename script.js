6e931870424281c292aef47420e14c5309751079
4444Fede <federico.vega@gm2dev.com>
2024-10-18 10:51:05 -0300

    feat(script.js): add exercise 2 function and users variable for testing

--- script.js
+++ script.js
@@ -19,3 +19,32 @@ console.log(sum(1, 2));
 console.log(sum(1)(2));
 console.log(sum(1, a));
 console.log(sum(1)(a));
+
+function hasLoggedInLast30Minutes(users) {
+  const now = new Date();
+  return users.some((user) => {
+    const diff = (now - user.login_time) / 1000 / 60;
+    return diff <= 30;
+  });
+}
+
+const users = [
+  {
+    id: "1",
+    login_time: new Date("2024-10-18T09:55:51.01"),
+  },
+  {
+    id: "2",
+    login_time: new Date("2024-10-18T09:30:51.01"),
+  },
+  {
+    id: "3",
+    login_time: new Date("2024-10-18T09:40:51.01"),
+  },
+  {
+    id: "4",
+    login_time: new Date("2024-10-18T10:30:31.01"),
+  },
+];
+
+console.log(hasLoggedInLast30Minutes(users));
